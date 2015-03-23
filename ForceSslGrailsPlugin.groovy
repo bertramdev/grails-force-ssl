bd3af50737b291b7687fbe6d2f6e70613b8d8af1
jerleng <jerleng@gmail.com>
2015-03-23 16:23:05 -0700

    bumped version to 1.0.1

--- ForceSslGrailsPlugin.groovy
+++ ForceSslGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import grails.util.Environment
 
 class ForceSslGrailsPlugin {
     // the plugin version
-    def version = "1.0.0"
+    def version = "1.0.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion   = "2.0 > *"
 
@@ -10,10 +10,13 @@ class ForceSslGrailsPlugin {
     def author = "David Estes"
     def authorEmail = "destes@bcap.com"
     def description = 'Creates a simple annotation to mark controller/actions as SSL restricted and performs the appropriate redirect.'
-
     def documentation   = "https://github.com/bertramdev/grails-force-ssl"
     def license = "APACHE"
     def organization = [name: "Bertram Labs", url: "http://www.bertramlabs.com/"]
+	def developers      = [
+		[name: 'David Estes', email: 'destes@bcap.com'],
+		[name: 'Jeremy Leng',email: 'jleng@bcap.com']]
+
     def issueManagement = [system: "GITHUB", url: "https://github.com/bertramdev/grails-force-ssl/issues"]
     def scm = [url: "https://github.com/bertramdev/grails-force-ssl"]
     def doWithSpring = {
